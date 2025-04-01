1b048d8c2193f1087f47e255a5da99071eb4d4ff
Matthew Mizelle <MIZELLMJ@dukes.jmu.edu>
2025-04-01 09:07:47 -0400

    update allowed hosts

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "mattmizellewebdev,127.0.0.1,localhost").split(",")
 
 
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
